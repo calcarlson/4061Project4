8e9b2e09be7e7ca3e472e31a18b75f4f9b9af46a
calcarlson <calcarlson@gmail.com>
2020-04-27 02:24:22 -0500

    working on server

--- template/p4/server/server.c
+++ template/p4/server/server.c
@@ -16,6 +16,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 //  -------------------- MAIN GOAL --------------------  //
 // The aim of the server is to maintain a global array   //
@@ -48,13 +49,11 @@ int updatedCount;
 pthread_mutex_t mutexUpdated;
 pthread_cond_t condUpdated;
 };
-
 // Global message buffer to store type and content of messages
 struct msessageBuffer{
     long messageType;
     char messageText[512];
 } message;
-
 // Global structure to store thread id information
 struct threadCount{
 pthread_t threads;
@@ -62,36 +61,17 @@ int clientID;
 struct wordCount* shared; 
 };
 
-// Process to create threads and assign IDs for use in 
-// the client program
-void* createThread(void* arg){
-    struct threadCount* process =(struct threadCount*) arg;
-    struct wordCount* shared = process->shared;
-    int clientID = process->clientID;
-
-    key_t key = ftok("queue", 8);
-    int msgq = msgget(key, IPC_CREAT | 0600);
-    printf("path %d", fileName);
-
-    while(true){
-        char *fileName = message.messageText;
-        if(strcmp(filename, "END")==0){
-            break;
-        }
-        printf("Received path %s", fileName);
-    }
-    printf("received END");
-}
-
-// function to open a file and read in the first letter of 
+// function to open a file and read in the first letter of
 // each word line by line into the word array
-void readFile(int clientID, char* fileName, int* letterOfLine){
-    FILE* fd = fopen(fileName, "r");
-    printf("Opening received file at %d",fileName);
-    while(true){
-        char* fileLine = NULL;
-        unsigned long n =0;
-        if(getline(&line, &n, fd) == -1){
+void readFile(int clientID, char *fileName, int *letterOfLine)
+{
+    FILE *fd = fopen(fileName, "r");
+    printf("Opening received file at %s", fileName);
+    while (true){
+        char *fileLine = NULL;
+        unsigned long n = 0;
+        if (getline(&fileLine, &n, fd) == -1)
+        {
             printf("getline");
             break;
         }
@@ -100,11 +80,62 @@ void readFile(int clientID, char* fileName, int* letterOfLine){
     fclose(fd);
 }
 
+// Process to create threads and assign IDs for use in 
+// the client program also translates the array to an output
+void* createThread(void* arg){
+    struct threadCount* process =(struct threadCount*) arg;
+    struct wordCount* shared = process->shared;
+    int clientID = process->clientID;
+    key_t key = ftok("queue", 8);
+    int msgq = msgget(key, IPC_CREAT | 0600);
+    while(true){
+        char *fileName = message.messageText;
+        if(strcmp(fileName, "END")==0){
+            break;
+        }
+        printf("Received path %s", fileName);
+        int output[26];
+        memset(output,0, 26);
+        readFile(clientID,fileName,output);
+//MUTEX LOCKS HERE MAYBE?
+// Iterate through the read file and copy info into wordcount struct
+        for(int i=0;i<26;i++){
+            shared->counter[i]+=output[i];
+        }
+        printf("ACK %s",fileName);
+        message.messageType=1025+clientID;
+        strcpy(message.messageText,"ACK");
+        msgsnd(msgq,&message,1024,0);
 
+    }
+    printf("received END");
+//decrement the count and broadcast thread status if last thread 
+    shared->updatedCount--;
+    if(shared->updatedCount == 0){
+        printf("last Thread");
+        pthread_cond_broadcast(&shared->condUpdated);
+    }
+    else{
+        printf("Waiting on shared->condUpdated");
+        while(shared->updatedCount!=0){
+            pthread_cond_wait(&shared->condUpdated,&shared->mutexUpdated);
+        }
+    }
+    printf("Output:");
+    char output[1024];
+    char* current = output;
+//Convert the array from readFile into an output for Client in the queue 
+    for(int i=0;i<26;i++){
+        current += sprintf(current,i==25?"%d":"%d#",shared->counter[i]);
+    }
+    message.messageType=1025+clientID;
+    strcpy(message.messageText,output);
+    msgsnd(msgq,&message,1024,0);
+    return NULL;
+}
 int main(int argc, char **argv)
 {
     printf(" -Server Started- \n");
-
 // Input argument validation
     if (argc > 2)
     {
@@ -117,20 +148,32 @@ int main(int argc, char **argv)
         exit(1);
     }
     int threadCounter = argc;
-
 // Creating an instant of thread struct for the thread counter
     struct threadCount threads[threadCounter];
-    struct wordCount words;
-
+    struct wordCount shared;
+// Set the array initialally to all zeroes before counting.
+    memset(shared.counter, 0, sizeof(shared.counter));
+// Create the number of threads defined by threadCounter 
+    shared.updatedCount = threadCounter;
+// Initialize the mutex objects required for server sync
+    pthread_mutex_init(&shared.mutexCounter,NULL);
+    pthread_mutex_init(&shared.mutexUpdated,NULL);
+    pthread_cond_init(&shared.condUpdated,NULL);
+// Create threads based on threadCounter(Argument passed to Server)   
     for(int i=0;i<threadCounter;i++){
         threads[i].clientID=i;
         threads[i].shared = &shared;
-        pthread_create(&threads[i].threads,NULL,&)
+        pthread_create(&threads[i].threads,NULL,&createThread,&threads[i]);
+    }
+// Wait for the created threads to be completed by Client
+    for(int i=0;i<threadCounter;i++){
+        pthread_join(threads[i].threads,NULL);
     }
 
-//set the array initialally to all zeroes before counting.
-    memset(words.count, 0, n*sizeof[words.count]);
-//create the number of threads defined by the threadcounter    
-    words.updatedCount=threadCounter;
+// End the created threads
+    pthread_mutex_destroy(&shared.mutexCounter);
+    pthread_mutex_destroy(&shared.mutexUpdated);
+    pthread_cond_destroy(&shared.condUpdated);
+
     printf(" -Server Ended- \n");
 }
