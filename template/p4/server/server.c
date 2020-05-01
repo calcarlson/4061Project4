17304a97e45270f57d0508b6e888ea63e51f8416
namht1999 <61252863+namht1999@users.noreply.github.com>
2020-04-30 22:01:15 -0500

    Update server.c

--- template/p4/server/server.c
+++ template/p4/server/server.c
@@ -3,6 +3,31 @@
 // Name : Conrad Carlson , Nam Trinh, Jessica Cunningham
 // x500 : Carl5362 , Namht1999, Cunni536
 
+//  -------------------- MAIN GOAL --------------------  //
+// The aim of the server is to maintain a global array   //
+// which stores the word count for each letter (how many //
+// words start with ‘a’, how many words start with       //
+// ‘b’, etc.). This array will be updated by its threads,//
+// where each thread receives some paths to text files   //
+// from a client process. The threads will modify the    //
+// array to reflect the number of words starting with a  //
+// letter in those files. When all the client processes  //
+// have finished sending up requests, the server threads //
+// send the final counts of the global array to          //
+// the clients.                                          //
+
+//  ---------------- PRINT STATEMENTS -----------------  //
+// 1) Server Start                                      -XX
+// 2) Each file path received (PID and path)            -XX
+// 3) Each Acknowledge to client "ACK" (PID and path)   -XX
+// 4) Each "END" received from Client (PID and path)     //
+// 5) After receiving “END” from client (Thread id)     -XX
+// 6) Server Ends                                       -XX
+
+// (Process ID = (0,1,...n-1) n=total number of threads) //
+
+// Global array structure to store word counts of each letter
+
 #include <stdio.h> 
 #include <sys/ipc.h> 
 #include <sys/msg.h> 
